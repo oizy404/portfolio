30a0166faea465710de0ab3df31a87a5ad3ea7b8
blz-mitz <mitz@blaze.online>
2024-10-10 19:11:57 +0800

    create pages and transitions when clicked

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 
 //components
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
+
 
 const jetbrainsMono = JetBrains_Mono({ 
   subsets: ["latin"],
@@ -20,8 +23,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetbrainsMono.variable}>
-        <Header/>
-        {children}</body>
+        <Header />
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
+        </body>
     </html>
   );
 }
