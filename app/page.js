903db5a5b6b43f9bf403c62187f371956bb6c81f
blz-mitz <mitz@blaze.online>
2024-10-12 20:39:12 +0800

    update homepage

--- app/page.js
+++ app/page.js
@@ -1,3 +1,50 @@
-export default function Home() {
-    return <main>Homepage</main>
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+//components
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Web Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br/> <span className="text-accent">Mitz Castillo</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>Download CV</span>
+                <FiDownload />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social 
+                  containerStyles="flex gap-6" 
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex
+                  justify-center items-center text-accent text-base hover:bg-accent
+                  hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+      </div>
+      <Stats />
+    </section>
+  )
 }
+
+export default Home
